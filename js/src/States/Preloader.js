0b6c34ab0143d5e89f03bbed138dd5f417ab0b46
Arlefreak <arlefreak@gmail.com>
2014-04-27 20:59:26 -0500

    Added sound effect

--- js/src/States/Preloader.js
+++ js/src/States/Preloader.js
@@ -8,8 +8,8 @@
 	Preload.prototype = {
 		preload: function() {
 			game.stage.backgroundColor = '#000';
-			this.preloadBarBg = this.add.sprite(300, 400, 'loadingBG');
-			this.preloadBar = this.add.sprite(304, 405, 'loading');
+			this.preloadBarBg = this.add.sprite(game.world.centerX, game.world.centerY, 'loadingBG');
+			this.preloadBar = this.add.sprite(game.world.centerX + 4, game.world.centerY + 5, 'loading');
 			game.load.audio('sfx', [ 'js/res/sound/die.mp3', 'js/res/sound/die.ogg' ]);
 			this.load.setPreloadSprite(this.preloadBar);
 			this.load.image('particle', 'js/res/img/particle.png');
